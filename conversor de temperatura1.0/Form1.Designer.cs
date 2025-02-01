3710a509682163b07d7b19c1b567fcb87fab1243
Angel Fernandez <Angel Fernandez@SAITAMA>
2025-01-31 22:18:46 -0600

    Agregar archivos de proyecto.

---
+++ conversor de temperatura1.0/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace conversor_de_temperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gcel = new System.Windows.Forms.TextBox();
+            this.gfa = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conversador de temperatura";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grados Celcius";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // gcel
+            // 
+            this.gcel.Location = new System.Drawing.Point(223, 194);
+            this.gcel.Name = "gcel";
+            this.gcel.Size = new System.Drawing.Size(139, 22);
+            this.gcel.TabIndex = 2;
+            // 
+            // gfa
+            // 
+            this.gfa.Location = new System.Drawing.Point(448, 194);
+            this.gfa.Name = "gfa";
+            this.gfa.Size = new System.Drawing.Size(126, 22);
+            this.gfa.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 56);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "C° A F°";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(350, 331);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 59);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "LIMPIAR";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(578, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 52);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "F° A C°";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(575, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Grados Farenheit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gfa);
+            this.Controls.Add(this.gcel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox gcel;
+        private System.Windows.Forms.TextBox gfa;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ conversor de temperatura1.0/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conversor_de_temperatura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gfa.Text = ((1.8 * double.Parse(gcel.Text)) + 32).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            gcel.Text = ((double.Parse(gfa.Text) - 32) / 1.8).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gfa.Text = ""; gcel.Text = "";
+            MessageBox.Show("se recetearon los valores de los textbox");
+        }
+
+        private void gcel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gfa_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
